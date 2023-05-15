e89cf79f2ad50cdc6315f458e14e7c69cf8ffc73
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-05-15 15:00:39 +0200

    react_components assigment done

---
+++ react_components_daren/src/App.jsx
@@ -0,0 +1,29 @@
+import HeaderText from "./components/HeaderText/HeaderText"
+import Card from "./components/Card/Card"
+import SecondaryHeader from './components/SecondaryHeaderText/SecondaryHeader'
+import Button from "./components/Button/Button"
+import Switcher from "./components/Switcher/Switcher"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <header>
+        <HeaderText/> <br />
+        <hgroup>
+          <Switcher/>
+          <SecondaryHeader/>
+          <Button/>
+        </hgroup>
+      </header>
+      <section className='container'>
+        <Card/>
+      </section>
+      
+    </>
+  )
+}
+
+export default App
