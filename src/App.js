329edbe524a93838063955767a27a6f682da8591
Gerti <onegerti@gmail.com>
2023-05-16 17:01:28 +0200

    new components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Header from "./Header";
-import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
   return (
     <>
       <Header />
-      <Navbar />
       <Home />
     </>
   );
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,21 @@ import "./Header.css";
 import React from "react";
 
 function Header() {
-  return <header class="main_header">Photo Fetcher</header>;
+  return (
+    <header class="main_header">
+      <p>Photo Fetcher</p>
+      <nav class="navbar">
+        <div class="switch_toogle">
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+        </div>
+        <p>Make photos grayscale</p>
+        <button class="newPhotos">Fetch New Photos</button>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
