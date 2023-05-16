1f51cb853dcc28d1369481323d162b2d76655f4d
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-05-16 15:35:19 +0200

    created react Page assigment, added switcher funcionality

--- daren-app/src/components/Switcher/Switcher.jsx
+++ daren-app/src/components/Switcher/Switcher.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Switcher.css"
+import { useGlobalContext } from '../../context/context'
 const Switcher = () => {
+  const {handleSwitcher}=useGlobalContext()
   return (
     <>
    <label class="form-switch">
         <input class="form-switch-input" type="checkbox" id="flexSwitchCheckDefault"/>
-        <span class="form-switch-span" for="flexSwitchCheckDefault"></span>
+        <span class="form-switch-span" for="flexSwitchCheckDefault" onClick={handleSwitcher}></span>
     </label>
     </>
   )
---
+++ daren-app/src/context/context.jsx
@@ -0,0 +1,21 @@
+import React, {useContext, useState} from "react";
+
+const AppContext= React.createContext()
+
+
+const AppProvider=({children})=>{
+    const [switcher,setSwitcher]=useState(false)
+
+    const handleSwitcher = () => {
+        setSwitcher(!switcher)
+      };
+    
+    return <AppContext.Provider value={{setSwitcher,handleSwitcher, switcher }}>
+        {children}
+    </AppContext.Provider>
+}
+export const useGlobalContext=()=>{
+    return useContext(AppContext)
+}
+
+export {AppProvider, AppContext}
