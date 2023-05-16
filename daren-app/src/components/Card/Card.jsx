e62a508522b2c05e4aa6e840fe67256a18175267
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-05-16 15:51:51 +0200

    added switch funcionality

--- daren-app/src/components/Card/Card.jsx
+++ daren-app/src/components/Card/Card.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./Card.css"
 import Image from "../../assests/images/img.jpg"
+import { useGlobalContext } from '../../context/context'
+
 const Card = () => {
+  const {switcher}=useGlobalContext()
+  
   const cardData=[
     { id: 1, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
     { id: 2, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
@@ -12,15 +16,13 @@ const Card = () => {
   const displayImages= cardData.map((card)=>(
      <>
      <article key={card.id} class="card">
-      <img class="card-image" src={card.img} alt="Background Image"/>
-
+      <img className={`card-image ${switcher?"image-grayscale":""}`} src={card.img} alt="Background Image"/>
        <figcaption class="card-overlay">
           {card.description}
       </figcaption>
      </article>
      </>
   ))
-
   return (
     <>
      {displayImages}
