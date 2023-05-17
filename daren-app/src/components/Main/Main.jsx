e3a58859e60791d91542759ef498af7baa1f6796
darenminarolli <80909893+darenminarolli@users.noreply.github.com>
2023-05-17 20:53:16 +0200

    added some changes

--- daren-app/src/components/Main/Main.jsx
+++ daren-app/src/components/Main/Main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import "./Main.css"
 import Card from '../Card/Card'
+import Image from "../../assests/images/img.jpg"
+import { useGlobalContext } from '../../context/context'
 const Main = () => {
+  const {switcher}=useGlobalContext()
+
+  const cardData=[
+    { id: 1, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
+    { id: 2, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
+    { id: 3, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
+    { id: 4, img: `${Image}`, description: 'Lorem ipsum dolor sit.' },
+  ]
+  const displayImages= cardData.map((card)=>(
+    <>
+     <Card id={card.id}  img={card.img} description={card.description} switcher={switcher}/>
+    </>
+ ))
   return (
     <>
      <main>
        <section className='container'>   
-          <Card/>
+         {displayImages}
         </section>
       </main>
     </>
