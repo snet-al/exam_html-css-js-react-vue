cb7575efcd6dfd28354c77aa8907f9167bbcd0f4
Gerti <onegerti@gmail.com>
2023-05-24 12:24:08 +0200

    pages added

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import Main from "../components/Main/Main";
+import Header from "../../components/Header/Header";
+import Main from "../../components/Main/Main";
 function HomePage() {
   return (
     <>
