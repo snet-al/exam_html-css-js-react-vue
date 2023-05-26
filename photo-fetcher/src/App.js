66c1dcf644a4d4a42d05c0a609fb081a77c83775
Juri15 <91764904+Juri15@users.noreply.github.com>
2023-05-26 20:27:48 +0200

    React-Photo-Fetcher

---
+++ photo-fetcher/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PhotoFetcher from "./components/PhotoFetcher";
+
+const App = () => {
+  return (
+    <div>
+      <PhotoFetcher />
+    </div>
+  );
+};
+
+export default App;
