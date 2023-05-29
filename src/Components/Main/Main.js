d44ca5c4c0fe690c6a849219576c192c60d11122
Gerti <onegerti@gmail.com>
2023-05-29 15:39:36 +0200

    changes

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
 import Figure from "../Figure/Figure";
 import data from "../../store/DummyData.json";
 
 function Main() {
-  const [authors, setAuthors] = useState(data);
+  const [authors, setAuthors] = useState();
+  useEffect(() => {
+    setAuthors(data);
+  }, []);
 
   return (
     <section className="body-section">
-      {authors.map((authors) => (
+      {authors?.map((authors) => (
         <Figure
           key={authors.id}
           id={authors.id}
