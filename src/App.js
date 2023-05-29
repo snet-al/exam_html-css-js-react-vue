06a2a89c50ece72f5007d6b97784621d76078401
kristiekonomi <82896722+kristiekonomi@users.noreply.github.com>
2023-05-29 15:47:38 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import PhotoGrid from './components/PhotoGrid/PhotoGrid';
-
+import Photo from './components/Photo/Photo';
 function App() {
   const handleToggle = () => {};
   const handleFetch = () => {};
@@ -11,22 +10,22 @@ function App() {
     <div className="main">
       <Header handleToggle={handleToggle} handleFetch={handleFetch} />
       <section className="grid-container">
-        <PhotoGrid
+        <Photo
           imageUrl="https://fastly.picsum.photos/id/49/200/200.jpg?hmac=w2wY6ewoLUCpaqq77gzCIdeKq2xKmTkssFtL_4K34H0"
           title="Lukas Budimaier"
           url="https://unsplash.com/photos/pwaaqfoMibi"
         />
-        <PhotoGrid
+        <Photo
           imageUrl="https://fastly.picsum.photos/id/455/200/200.jpg?hmac=YZhCbBjCYF0ha5dR9ElToDVwWcw05w0e4pAv5S9nZYg"
           title="Lukas Budimaier"
           url="https://unsplash.com/photos/pwaaqfoMibi"
         />
-        <PhotoGrid
+        <Photo
           imageUrl="https://fastly.picsum.photos/id/435/200/200.jpg?hmac=yk7-HtvV0x2Z6OB4YhbyAbYxX0nQQCNTzs_MgegSkcE"
           title="Lukas Budimaier"
           url="https://unsplash.com/photos/pwaaqfoMibi"
         />
-        <PhotoGrid
+        <Photo
           imageUrl="https://fastly.picsum.photos/id/499/200/200.jpg?hmac=6OuceXs-LrhY-PtwyqBs_bARD8YWZEj9XuZ_ZY894Wc"
           title="Lukas Budimaier"
           url="https://unsplash.com/photos/pwaaqfoMibi"
