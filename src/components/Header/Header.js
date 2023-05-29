35a67124f4c53715b47e4e53c1f2e9dcb7a29a1b
kristiekonomi <82896722+kristiekonomi@users.noreply.github.com>
2023-05-29 10:45:49 +0200

    update 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
+
 import React from 'react';
 import './Header.css';
 import Toggle from '../Toggle/Toggle.js';
 
 function Header({ handleToggle, handleFetch }) {
   return (
-      <div className="row">
-        <Toggle onChange={handleToggle} />
-        <h5 className="toggletext">Make photos grayscale</h5>
-        <button className="button" onClick={handleFetch}>
-          Fetch New Photos
-        </button>
-      </div>
+    <div className="row">
+      <Toggle onChange={handleToggle} />
+      <h5 className="toggletext">Make photos grayscale</h5>
+      <button className="button" onClick={() => handleFetch()}>
+        Fetch New Photos
+      </button>
+    </div>
   );
 }
 
 export default Header;
+
+
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,31 +1,52 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomePage.css';
-import { photos } from '../../store/dummyData';
 import Header from '../../components/Header/Header';
 import PhotoGrid from '../../components/PhotoGrid/PhotoGrid';
 
 function HomePage() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  const fetchPhotos = () => {
+    const apiUrl = 'https://picsum.photos/v2/list?limit=4';
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        setPhotos(data);
+      })
+      .catch(error => {
+        console.error('Error fetching photos:', error);
+      });
+  };
+
   const handleToggle = () => {};
-  const handleFetch = () => {};
+
+  const handleFetch = () => {
+    fetchPhotos();
+  };
 
   return (
     <div className="main">
       <Header handleToggle={handleToggle} handleFetch={handleFetch} />
       <section className="grid-container">
-         {photos.map((photo) => (
-           <PhotoGrid
-             key={photo.id}
-             imageUrl={photo.imageUrl}
-             title={photo.title}
+        {photos.map(photo => (
+          <PhotoGrid
+            key={photo.id}
+            imageUrl={photo.download_url}
+            title={photo.author}
             url={photo.url}
-           />
-         ))}
-       </section>
-
+          />
+        ))}
+      </section>
     </div>
   );
 }
+
 
 export default HomePage;
 
