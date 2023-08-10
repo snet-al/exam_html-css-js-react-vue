2ce51d108dea333bd51e3db69fd8683cc35bc02a
Xheni <80475264+xhenisa22@users.noreply.github.com>
2023-08-10 12:39:42 +0200

    Adding dummy data

--- task-components/src/components/UI/ImagePicker.js
+++ task-components/src/components/UI/ImagePicker.js
@@ -1,47 +1,22 @@
 import React from "react";
 import "../../App.css";
-import image1 from "../../image1.jpg";
+import dataDummy  from "../../STORE/data";
 
 const ImagePicker = () => {
   return (
     <div>
       <div className="container">
-        <div className="column">
-          <img src={image1} alt="Image 1" />
-          <div className="image-information">
-            <p className="author-name">Lukas Budimaier</p>
-            <a className="image-url" href="https://unsplash.com/photos/pwaaqfoMibl">
-              https://unsplash.com/photos/pwaaqfoMibl
-            </a>
+        {dataDummy .map((data, index) => (
+          <div className="column" >
+            <img src={data.image} />
+            <div className="image-information">
+              <p className="author-name">{data.name}</p>
+              <a className="image-url" href={data.link}>
+                {data.link}
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="column">
-          <img src={image1} alt="Image 1" />
-          <div className="image-information">
-            <p className="author-name">Lukas Budimaier</p>
-            <a className="image-url" href="https://unsplash.com/photos/pwaaqfoMibl">
-              https://unsplash.com/photos/pwaaqfoMibl
-            </a>
-          </div>
-        </div>
-        <div className="column">
-          <img src={image1} alt="Image 1" />
-          <div className="image-information">
-            <p className="author-name">Lukas Budimaier</p>
-            <a className="image-url" href="https://unsplash.com/photos/pwaaqfoMibl">
-              https://unsplash.com/photos/pwaaqfoMibl
-            </a>
-          </div>
-        </div>
-        <div className="column">
-          <img src={image1} alt="Image 1" />
-          <div className="image-information">
-            <p className="author-name">Lukas Budimaier</p>
-            <a className="image-url" href="https://unsplash.com/photos/pwaaqfoMibl">
-              https://unsplash.com/photos/pwaaqfoMibl
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
