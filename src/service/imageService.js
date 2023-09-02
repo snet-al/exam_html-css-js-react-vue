03935794623c4859133fcbb9d8bebd7c26f63130
Xheni <80475264+xhenisa22@users.noreply.github.com>
2023-09-02 22:57:26 +0200

    Fixes

--- src/service/imageService.js
+++ src/service/imageService.js
@@ -1,5 +1,5 @@
 export const fetchRandomImages = (pageNumber) => {
-  const LIMIT=4;
+  const LIMIT = 4;
   return fetch(`https://picsum.photos/v2/list?page=${pageNumber}&limit=${LIMIT}`)
     .then((response) => {
       console.log(response);
