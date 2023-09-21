f50608f2163f351e16e14f3e37b9b0b3adb35c9e
LedinaShahi <ledina.shahi@fshnstudent.info>
2023-09-21 11:34:50 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import ImageContainer from './pages/ImageContainer';
+import ImageContainer from './pages/ImageGalleryPage';
 import Footer from './components/Footer';
 
 function App() {
