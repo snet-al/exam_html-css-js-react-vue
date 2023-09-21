324fd1d7286b7e4c12696f06bf433c3630458844
LedinaShahi <ledina.shahi@fshnstudent.info>
2023-09-21 11:39:29 +0200

    lastChanges

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import ImageContainer from './pages/ImageGalleryPage';
+import ImageGalleryPage from './pages/ImageGalleryPage';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
      <Header/>
-     <ImageContainer/>
+     <ImageGalleryPage/>
      <Footer/>
     </div>
   );
