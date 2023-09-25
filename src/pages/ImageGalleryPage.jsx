5fad92b74afe7c26adb4ec847decbefcae7db10d
LedinaShahi <ledina.shahi@fshnstudent.info>
2023-09-25 16:34:41 +0200

    changes

--- src/pages/ImageGalleryPage.jsx
+++ src/pages/ImageGalleryPage.jsx
@@ -4,19 +4,16 @@ import MainLayout from "../layout/MainLayout";
 import { dummyData } from "../store/data";
 
 const ImageGalleryPage = () => {
-  const renderImageItems = () => {
-    return dummyData.map((data) => (
-      <ImageItem key={data.id} data={data} />
-    ));
-  };
-
+  
   return (
     <MainLayout>
       <section className="image-container">
-        {renderImageItems()}
+        {dummyData.map((data) => (
+            <ImageItem key={data.id} data={data}/>
+        ))
+      };
       </section>
     </MainLayout>
-  );
-};
+)};
 
 export default ImageGalleryPage;
