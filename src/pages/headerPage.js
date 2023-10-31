0c92a4bf1012c3814231c8e748b6cf4e58f26c9e
Eri Koleci <kolecieri@gmail.com>
2023-10-31 15:18:31 +0100

    add page

---
+++ src/pages/headerPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import "../index.css";
+
+function headerPage() {
+  return (
+    <div>
+      <h1>Header Page</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default headerPage;
