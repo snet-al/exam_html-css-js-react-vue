46f66dc5a637fae887ba407691c4ad2cdc5c9aff
flavio1323 <flaviobregu1@gmail.com>
2025-03-18 17:46:35 -0500

    Convert HTML to React components and put into UI folder

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./UI/Button";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Button text="More Photos" className="load-more" />
+        </footer>
+    );
+}
+
+export default Footer;
