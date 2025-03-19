e60d11f45742a4bf71aba562ff4c664158ed5e79
flavio1323 <flaviobregu1@gmail.com>
2025-03-18 19:13:30 -0500

    Add store folder and implement dummyData for dynamic content

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import "./HomePage.css";
 import ImageContainer from "../components/ImageContainer/ImageContainer";
-
-const images = [
-    { id: 1, src: "https://picsum.photos/200", alt: "Photo 1" },
-    { id: 2, src: "https://picsum.photos/201", alt: "Photo 2" },
-    { id: 3, src: "https://picsum.photos/202", alt: "Photo 3" },
-    { id: 4, src: "https://picsum.photos/203", alt: "Photo 4" },
-];
+import dummyData from "../store/dummyData";
 
 function HomePage() {
     return (
         <section className="image-grid">
-            {images.map((image) => (
+            {dummyData.map((image) => (
                 <figure key={image.id}>
                     <ImageContainer src={image.src} alt={image.alt} />
                 </figure>
---
+++ src/store/dummyData.jsx
@@ -0,0 +1,8 @@
+const dummyData = [
+    { id: 1, src: "https://picsum.photos/200", alt: "Photo 1" },
+    { id: 2, src: "https://picsum.photos/201", alt: "Photo 2" },
+    { id: 3, src: "https://picsum.photos/202", alt: "Photo 3" },
+    { id: 4, src: "https://picsum.photos/203", alt: "Photo 4" },
+];
+
+export default dummyData;
