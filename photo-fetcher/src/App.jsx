4347a70281cb9f6278a1d4be5afd2d790021a642
mpetrit <180044841+mpetrit@users.noreply.github.com>
2025-05-29 13:30:48 +0200

    Added the PAGES folder with MainPage

--- photo-fetcher/src/App.jsx
+++ photo-fetcher/src/App.jsx
@@ -1,15 +1,9 @@
-import Footer from './components/Footer.jsx'
-import Header from './components/Header.jsx'
-import Main from './components/Main.jsx'
+import MainPage from './PAGES/MainPage'
 
 function App() {
 
   return (
-    <div className='container'>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+    <MainPage/>
   )
 }
 
---
+++ photo-fetcher/src/PAGES/MainPage.jsx
@@ -0,0 +1,16 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Main from '../components/Main'
+
+function MainPage() {
+
+  return (
+    <div className='container'>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default MainPage
