0d097df6545d4ee162e8383ffd6d468832180b8b
Heartlez <kevinmatka@hotmail.com>
2025-07-03 14:23:38 +0200

    Fix

--- src/Components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(props) {
-    return(
-    <header>
-        <h2>{props.value}</h2>
-    </header>
-    )
-}
-
-export default Header
--- src/Components/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button(props){
-  return(
-    <button
-     className={props.className} id={props.id} onClick={props.onClick}>{props.text}
-    </button>
-  );
-}
-
-export default Button
