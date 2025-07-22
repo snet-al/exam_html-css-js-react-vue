c4bf89c35692103f31a7ed27b2ca3c3ce9a4b724
KF <kfaber@terpmail.umd.edu>
2025-07-22 13:22:59 +0200

    Fixed up functionality, added styling (although incomplete), added switch from Material Design component library

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 "use strict"
 
 let pictureIDs = []
-let gray = false
+
+// borrowed component from Google's material design library
+let graySwitch = document.getElementById("switch")
+let addPicsButton = document.getElementById("add-pics")
+let newPicsButton = document.getElementById("new-pics")
+
+let width = 351
+let height = 250
+
+graySwitch.addEventListener("click", grayscale)
+addPicsButton.addEventListener("click", addPics)
+newPicsButton.addEventListener("click", genNewPics)
+
+addPics()
 
 function pictureBuilder(finish, id) {
     let pic = document.getElementById(`picture${id}`)
@@ -14,6 +27,43 @@ function pictureBuilder(finish, id) {
     return pic
 }
 
+async function frameBuilder(pic, id) {
+    let frame = pic.parentElement
+
+    if (frame == null) {
+        frame = document.createElement("div")
+        frame.classList.add("picture-frame")
+        frame.appendChild(pic)
+
+        let overlay = document.createElement("div")
+
+        let authname = document.createElement("h3")
+        let link = document.createElement("a")
+
+        overlay.appendChild(authname)
+        overlay.appendChild(link)
+        overlay.classList.add("overlay")
+        frame.appendChild(overlay)
+    }
+
+    let data = await fetch(`https://picsum.photos/id/${id}/info`)
+    let jsonData = await data.json() 
+
+    let overlay = frame.getElementsByClassName("overlay")[0]
+    for (let child of overlay.children) {
+        if (child.tagName == "H3") {
+            // console.log("weep")
+            child.textContent = jsonData.author
+        } else if (child.tagName == "A") {
+            // console.log("wope")
+            child.href = jsonData.url
+            child.textContent = jsonData.url
+        }
+    }
+
+    return frame
+}
+
 function linkBuilder(width, height, id, grayscale) {
     let link = "https://picsum.photos"
 
@@ -21,47 +71,55 @@ function linkBuilder(width, height, id, grayscale) {
         link = `${link}/id/${id}`
     }
 
-    link = `${link}/${width}/${height}.webp?random=${Date.now()}`
+    link = `${link}/${width}/${height}.jpg`
 
     if (grayscale) {
-        link = `${link}&grayscale`
+        link = `${link}?grayscale`
     }
 
     return link
 }
 
 async function genNewPics() {
-    gray = false
+    graySwitch.selected = false
 
-    let reqs = pictureIDs.map(_ => fetch(linkBuilder(355, 250, null, gray)))
+    let reqs = pictureIDs.map(_ => fetch(linkBuilder(width, height, null, graySwitch.selected)))
 
     let results = await Promise.all(reqs)
     let resBlobs = await Promise.all(
         results.map(r => r.blob())
     )
 
-    results.forEach(async (resBody, index) => {
-        let resBlob = resBlobs[index]
-
+    let frames = results.map(async (resBody, index) => {
+        // let resBlob = await resBody.blob()
         let newId = resBody.headers.get("Picsum-ID")
-
-        let pic = pictureBuilder(resBlob, pictureIDs[index])
+        let pic = pictureBuilder(resBlobs[index], pictureIDs[index])
         pic.id = `picture${newId}`
         pictureIDs[index] = newId
+        return frameBuilder(pic, newId)
     })
+    await Promise.all(frames)
+
+    // results.forEach((resBody, index) => {
+    //     let resBlob = resBlobs[index]
+
+    //     let newId = resBody.headers.get("Picsum-ID")
+
+    //     let pic = pictureBuilder(resBlob, pictureIDs[index])
+    //     pic.id = `picture${newId}`
+    //     pictureIDs[index] = newId
+    // })
 }
 
 async function grayscale() {
-    gray = !gray
-
-    let reqs = pictureIDs.map(x => fetch(linkBuilder(355, 250, x, gray)))
+    let reqs = pictureIDs.map(x => fetch(linkBuilder(width, height, x, !graySwitch.selected)))
 
     let results = await Promise.all(reqs)
     let resBlobs = await Promise.all(
         results.map(r => r.blob())
     )
 
-    results.forEach(async (resBody, index) => {
+    results.forEach((resBody, index) => {
         let resBlob = resBlobs[index]
 
         let id = resBody.headers.get("Picsum-ID")
@@ -70,9 +128,10 @@ async function grayscale() {
     })
 }
 
-async function main() {
-    let reqs = [0, 0, 0, 0].map(_ => fetch(linkBuilder(355, 250, null, gray)))
+async function addPics() {
+    let reqs = [0, 0, 0, 0].map(_ => fetch(linkBuilder(width, height, null, graySwitch.selected)))
     
+    // i should probably do changes by chunks
     let results = await Promise.all(reqs)
     let resBlobs = await Promise.all(
         results.map(r => r.blob())
@@ -87,12 +146,8 @@ async function main() {
 
         let pic = pictureBuilder(resBlob, id)
 
-        let frame = document.createElement("div")
-        frame.classList.add("picture-frame")
-        frame.appendChild(pic)
+        let frame = await frameBuilder(pic, id)
 
         container.appendChild(frame)
     })
 }
-
-main()
