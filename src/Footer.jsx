68f16a534e4cd02fa509027868b4d738ccf50527
3rg15 <ergishasanbelli@gmail.com>
2025-08-06 14:42:04 +0200

    Step2 and 2.1
    Started ReactxVite/implemented React components

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./UI/Button";
+
+const Footer = () => {
+    return (
+        <footer className="app-footer">
+            <Button text="Load More Images" className="load-more-btn" />
+        </footer>
+    );
+}
+
+export default Footer;
