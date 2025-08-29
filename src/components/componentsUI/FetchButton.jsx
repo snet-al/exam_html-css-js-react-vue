10def48196f3786821e17147f3da09956feb114d
Sildi Sherolli <107214623+sildi-sh@users.noreply.github.com>
2025-08-29 16:40:24 +0200

    Delete src directory

--- src/components/componentsUI/FetchButton.jsx
+++
@@ -1,9 +0,0 @@
-import './FetchButton.css'
-
-export function FetchButton() {
-  return (
-    <div className="fetch-button-div">
-      <button className="fetch-button">Fetch New Photos</button>
-    </div>
-  )
-}
