d0ab16973bb3c788b68fcae289c460c219408f70
Sildi Sherolli <107214623+sildi-sh@users.noreply.github.com>
2025-09-01 16:01:20 +0200

    Komponentet e rinj pas ndryshimeve

---
+++ src/components/componentsUI/FetchButton.jsx
@@ -0,0 +1,7 @@
+import './FetchButton.css'
+
+export function FetchButton() {
+  return (
+      <button className="fetch-button">Fetch New Photos</button>
+  )
+}
---
+++ src/components/componentsUI/Header.jsx
@@ -0,0 +1,8 @@
+import './Header.css'
+export function Header() {
+  return(
+    <h1>
+      Photo fetcher
+    </h1>
+  )
+}
