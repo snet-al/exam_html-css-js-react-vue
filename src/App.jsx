035db902b01a042d9c7996496e2bfecad5636f43
Sildi Sherolli <107214623+sildi-sh@users.noreply.github.com>
2025-09-01 16:40:31 +0200

    Layout pas ndryshimeve

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Content } from './pages/Content'
+import { Layout } from './layout/Layout'
+import './App.css'
+
+function App() {
+
+  return (
+    <Layout>
+      <Content />
+    </Layout>  
+  )
+}
+
+export default App
