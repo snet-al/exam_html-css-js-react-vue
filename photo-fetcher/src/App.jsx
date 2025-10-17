d99e1b2d0bd5d1b4bc11b6e90f46e5375377736e
Atea Caslli <ateacaslli1@gmail.com>
2025-10-17 13:00:55 +0200

    Step 4: Add Layout

--- photo-fetcher/src/App.jsx
+++ photo-fetcher/src/App.jsx
@@ -1,12 +1,13 @@
-import Header from "./components/Header";
+import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import "./index.css";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Home />
+      <Layout>
+        <Home />
+      </Layout>
     </>
   );
 }
--- photo-fetcher/src/pages/Home.jsx
+++ photo-fetcher/src/pages/Home.jsx
@@ -2,10 +2,8 @@ import Gallery from "../components/Gallery";
 
 export default function Home() {
   return (
-    <main className="container">
       <section className="gallery-section">
         <Gallery />
       </section>
-    </main>
   );
 }
