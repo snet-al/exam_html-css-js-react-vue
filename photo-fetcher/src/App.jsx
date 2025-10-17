514d3c42cd3b357860d88680f13970c0424c1b7b
Atea Caslli <ateacaslli1@gmail.com>
2025-10-17 12:45:25 +0200

    Step 3: Introduce pages folder and Home page

--- photo-fetcher/src/App.jsx
+++ photo-fetcher/src/App.jsx
@@ -1,13 +1,12 @@
 import Header from "./components/Header";
-import Gallery from "./components/Gallery";
+import Home from "./pages/Home";
+import "./index.css";
 
 export default function App() {
   return (
     <>
       <Header />
-      <main className="container">
-        <Gallery />
-      </main>
+      <Home />
     </>
   );
 }
---
+++ photo-fetcher/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Gallery from "../components/Gallery";
+
+export default function Home() {
+  return (
+    <main className="container">
+      <section className="gallery-section">
+        <Gallery />
+      </section>
+    </main>
+  );
+}
