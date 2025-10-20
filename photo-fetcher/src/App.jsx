0dda8d61f55ff25d0791a47066dde0911a45e9e7
Atea Caslli <ateacaslli1@gmail.com>
2025-10-20 18:22:57 +0200

    Step 6: Add services/photos API

--- photo-fetcher/src/App.jsx
+++ photo-fetcher/src/App.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import { dummyData } from "./store/dummyData";
+import { fetchFourPhotos } from "./services/photos";
 import "./index.css";
 
 export default function App() {
-  const [photos, setPhotos] = useState(dummyData);
+  const [photos, setPhotos] = useState([]);
   const [grayscaleOn, setGrayscaleOn] = useState(false);
 
   function handleToggle(e) {
     setGrayscaleOn(e.target.checked);
   }
 
-  function handleFetchNew() {
-    const next = [...dummyData].sort(() => Math.random() - 0.5);
-    setPhotos(next);
+  async function handleFetchNew() {
+    const items = await fetchFourPhotos();
+    setPhotos(items);
   }
+
+  useEffect(() => {
+    handleFetchNew();
+  }, []);
 
   return (
     <Layout
