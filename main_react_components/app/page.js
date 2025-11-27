046cdbfe11a2f135e16c2804adbc323126d18055
ITZ-Juri <wapex999@gmail.com>
2025-11-27 11:08:47 +0100

    Separating UI components

--- main_react_components/app/page.js
+++ main_react_components/app/page.js
@@ -1,6 +1,8 @@
+import PhotoFetcher from "@/components/UI/photo_fetcher";
+
 export default function Home() {
 
-  const id = [1,2,3,4]
+  
 
   return (
     <div className="max-w-[750px] justify-self-center">
@@ -24,41 +26,7 @@ export default function Home() {
       </div>
 
       {/*photo fetcher*/}
-        <div className="grid grid-cols-2 gap-4">
-
-            <div className="relative">
-              <img src={`https://picsum.photos/id/237/367/280`} className="h-full w-full"/>
-              <div className="absolute bg-black text-white bottom-0.5 w-full p-2 opacity-70">
-                <p>Author</p>
-                <p>https://picsum.photos/id/237/367/280</p>
-              </div>
-            </div>
-
-            <div className="relative">
-              <img src={`https://picsum.photos/id/237/367/280`} className="h-full w-full"/>
-              <div className="absolute bg-black text-white bottom-0.5 w-full p-2 opacity-70">
-                <p>Author</p>
-                <p>https://picsum.photos/id/237/367/280</p>
-              </div>
-            </div>
-
-            <div className="relative">
-              <img src={`https://picsum.photos/id/237/367/280`} className="h-full w-full"/>
-              <div className="absolute bg-black text-white bottom-0.5 w-full p-2 opacity-70">
-                <p>Author</p>
-                <p>https://picsum.photos/id/237/367/280</p>
-              </div>
-            </div>
-
-            <div className="relative">
-              <img src={`https://picsum.photos/id/237/367/280`} className="h-full w-full"/>
-              <div className="absolute bg-black text-white bottom-0.5 w-full p-2 opacity-70">
-                <p>Author</p>
-                <p>https://picsum.photos/id/237/367/280</p>
-              </div>
-            </div>
-
-        </div>
+        <PhotoFetcher/>
     </div>
   );
 }
