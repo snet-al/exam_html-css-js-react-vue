d0530abc97fabe420d94754050d60564937b1285
ITZ-Juri <wapex999@gmail.com>
2025-11-28 15:54:36 +0100

    implemented api

--- main_react_components/components/UI/photo_fetcher.js
+++ main_react_components/components/UI/photo_fetcher.js
@@ -1,21 +1,29 @@
-import { dummyData } from "@/store/data"
+"use client"
+import { useEffect, useState } from "react"
+import fetch_photos from "@/services/apiFetcher"
 
 export default function PhotoFetcher() {
-    const id = [1, 2, 3, 4]
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        async function getPics() {
+            const photos = await fetch_photos()
+            setData(photos)
+        }
+        getPics()
+    })
+
     return (
         <div className="grid grid-cols-2 gap-4">
-            {id.map((id) => (
-                <div key={id} className="relative">
-                    <img src={dummyData[id-1]} className="h-full w-full" />
+            {data.map((pic) => (
+                <div key={pic.id} className="relative">
+                    <img src={pic.download_url} className="h-full w-full" />
                     <div className="absolute bg-black text-white bottom-0.5 w-full p-2 opacity-70">
-                        <p>Author</p>
-                        <p>{dummyData[id-1]}</p>
+                        <p>{pic.author}</p>
+                        <p>{pic.download_url}</p>
                     </div>
                 </div>
             ))}
-
         </div>
-
-
     )
 }
