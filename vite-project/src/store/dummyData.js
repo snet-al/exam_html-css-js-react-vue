d009aed5164a10bb42eb8e149f28d0a22bf47bad
Heliomyr <myrteza.helio@gmail.com>
2026-02-27 15:13:08 +0100

    React Store

---
+++ vite-project/src/store/dummyData.js
@@ -0,0 +1,6 @@
+export const dummyData = [
+  { id: "1", author: "Author 1", url: "https://picsum.photos/", download_url: "https://picsum.photos/seed/1/800/600" },
+  { id: "2", author: "Author 2", url: "https://picsum.photos/", download_url: "https://picsum.photos/seed/2/800/600" },
+  { id: "3", author: "Author 3", url: "https://picsum.photos/", download_url: "https://picsum.photos/seed/3/800/600" },
+  { id: "4", author: "Author 4", url: "https://picsum.photos/", download_url: "https://picsum.photos/seed/4/800/600" },
+];
