dc0a5f713e8c28494e1d43cbd510627c2be6f1a4
Ersi-01 <ersideda33@gmail.com>
2026-06-02 12:21:52 +0200

    feat: migrate static HTML into  React components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
